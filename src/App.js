fefa4fc11cafa5943656278a5d027f29e71a9f7c
Enrique726 <loverthherrera7@gmail.com>
2024-12-10 11:47:28 +0100

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RealEstateApp from './components/RealEstateApp';
+
+function App() {
+  return (
+    <div className="App">
+      <RealEstateApp />
+    </div>
+  );
+}
+
+export default App;
